e63d84c8cd0c6f38878ecce8ae0981af423354ad
Shushant-Priyadarshi <shushantgroza@gmail.com>
2024-04-23 01:05:43 +0530

    iniital commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const crypto = require("crypto");
+
+function hashPassword(password, salt) {
+  const hash = crypto.createHash("sha256");
+
+  hash.update(password + salt);
+
+  const hashedPassword = hash.digest("hex");
+
+  return hashedPassword;
+}
+
+function generateSalt(length = 16) {
+  return crypto.randomBytes(length).toString("hex");
+}
+
+module.exports = {
+  hashPassword,
+  generateSalt,
+};
